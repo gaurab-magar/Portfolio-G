bbc44bb02597c47338f1b1c63d18879ec1454b0e
HP <gdikshan10@gmail.com>
2023-12-23 13:31:33 +0545

    bootstrap and scss implemented not dynamic for frotend

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let backtotop = document.querySelector('.back-to-top')
+if (backtotop) {
+  const toggleBacktotop = () => {
+    if (window.scrollY > 100) {
+      backtotop.classList.add('active')
+    } else {
+      backtotop.classList.remove('active')
+    }
+  }
+  window.addEventListener('load', toggleBacktotop)
+  onscroll(document, toggleBacktotop)
+}
